703191fe8655b5d1100fb76f2514c76fd4f956d5
JhaAakri <aditijha0830@gmail.com>
2025-12-18 16:19:49 +0530

    Initial commit - InfiAlgo project

--- components/Header/menuData.tsx
+++ components/Header/menuData.tsx
@@ -1,9 +1,11 @@
 const menuData = [
   { id: 1, title: "Home", path: "/" },
-  { id: 2, title: "Features", path: "#" },
-  { id: 3, title: "Dashboard", path: "#" },
-  { id: 4, title: "Learn", path: "#" },
-  { id: 5, title: "Security", path: "#" },
+  { id: 2, title: "About Us", path: "#" },
+  { id: 3, title: "Services", path: "#" },
+  { id: 4, title: "Payment", path: "#" },
+  { id: 5, title: "Blog", path: "#" },
+  { id: 5, title: "Start Virtual Tour", path: "#" },
+
 ];
 
 export default menuData;
